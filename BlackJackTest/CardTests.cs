f8185e4c303e1fa366953d025bc0161d7641b91a
Logan Connor <LoganConnor44@gmail.com>
2018-03-27 19:30:51 +0000

    Added Tests For Card Class

---
+++ BlackJackTest/CardTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlackJack;
+
+namespace BlackJackTest {
+    [TestClass]
+    public class CardTests {
+        [TestMethod]
+        public void TestInstantiation() {
+            Card card = new Card(4, 'H');
+            Assert.IsInstanceOfType(card, typeof(Card));
+        }
+        [TestMethod]
+        public void TestSetFaceCards() {
+            Card Ace = new Card(1, 'H');
+            Assert.AreEqual("Ace", Ace.Face);
+
+            Card Jack = new Card(11, 'H');
+            Assert.AreEqual("Jack", Jack.Face);
+
+            Card Queen = new Card(12, 'H');
+            Assert.AreEqual("Queen", Queen.Face);
+
+            Card King = new Card(13, 'H');
+            Assert.AreEqual("King", King.Face);
+        }
+    }
+}
