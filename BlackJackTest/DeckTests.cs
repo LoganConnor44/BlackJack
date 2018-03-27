fcfc0859180b5aa7c8cf3869e204c66ab9eb1a98
Logan Connor <LoganConnor44@gmail.com>
2018-03-27 20:05:16 +0000

    Finalized card tests and began deck testing

---
+++ BlackJackTest/DeckTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlackJack;
+
+namespace BlackJackTest {
+    [TestClass]
+    public class DeckTests {
+        [TestMethod]
+        public void TestInstantiation() {
+            Deck deck = new Deck();
+            Assert.IsInstanceOfType(deck, typeof(Deck));
+        }
+        [TestMethod]
+        public void TestTotalNumberOfCardsInDeck() {
+            Deck deck = new Deck();
+            Assert.AreEqual(52, deck.Cards.Count);
+        }
+    }
+}
